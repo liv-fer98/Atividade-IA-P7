cb831bd0821f510ba510100a1fe13d5bdff17bf7
liv-fer98 <83189090+liv-fer98@users.noreply.github.com>
2024-02-29 17:02:25 -0300

    Update exe01_IA.py
    Adição de comentários

--- exe01_IA.py
+++ exe01_IA.py
@@ -1,8 +1,13 @@
-#Sistema de Sistema Simplificado de Streaming de Vídeo
+#Sistema Simplificado de Streaming de Vídeo
 
+#Criar uma lista de vídeos
 filmes = ['lua', 'sol']
 series = ['yin', 'yang']
 
+"""
+Foi criado uma função para exibir todos os itens de cada lista,
+utilizou-se o loop -for- para processar a lista de filmes e de séries
+"""
 def catalogo(filmes, series):
     print("===================")
     print("Catálogo de filmes:")
@@ -14,6 +19,12 @@ def catalogo(filmes, series):
         print(listar_series)
     print("===================")
 
+'''
+-> A função  abaixo tem como objetivo exibir o catálogo e permitir que o usuário escolha o que deseja assistir entre as opções;
+-> Após chamar a função de catálogo um contador foi adicionado para ser usado como condição do laço de repetição -while-;
+-> O -input- permite ao sistema interagir com o usuário e dependendo da entrada a função condicional -if e else- responderá;
+-> Enquanto o usuário digitar filmes fora do catálogo o sistema permitirá uma nova escolha.
+'''
 def iniciar_streaming(self):
     catalogo(filmes, series)
     count = 1
@@ -30,5 +41,5 @@ def iniciar_streaming(self):
             print('    Escolha entre as nossas outras opções!')
             print('**********************************************\n')
       
-
+#Chamando a função iniciadora do sistema
 iniciar_streaming(catalogo)
